b93a6b1045d6eb07f202dce9990c9c4cdbf36d3d
dzaragoza <a@a.com>
2019-12-11 00:21:37 +0100

    Refactor 3

--- 3/3a.py
+++ 3/3a.py
@@ -3,61 +3,41 @@ f = open('input.txt').readlines()
 a = [(i[0],int(i[1:])) for i in f[0].split(',')]
 b = [(i[0],int(i[1:])) for i in f[1].split(',')]
 
-x = 0
-y = 0
+def travel(wire, layout, value):
+    x = 0
+    y = 0
+
+    for i in wire:
+        if i[0] == 'U':
+            for j in range(i[1]):
+                y += 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+        elif i[0] == 'D':
+            for j in range(i[1]):
+                y -= 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+        elif i[0] == 'L':
+            for j in range(i[1]):
+                x -= 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+        elif i[0] == 'R':
+            for j in range(i[1]):
+                x += 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
 
 layout = {}
-
-for i in a:
-    if i[0] == 'U':
-        for j in range(i[1]):
-            y += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-    elif i[0] == 'D':
-        for j in range(i[1]):
-            y -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-    elif i[0] == 'L':
-        for j in range(i[1]):
-            x -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-    elif i[0] == 'R':
-        for j in range(i[1]):
-            x += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-    
-x = 0
-y = 0
-    
-for i in b:
-    if i[0] == 'U':
-        for j in range(i[1]):
-            y += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-    elif i[0] == 'D':
-        for j in range(i[1]):
-            y -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-    elif i[0] == 'L':
-        for j in range(i[1]):
-            x -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-    elif i[0] == 'R':
-        for j in range(i[1]):
-            x += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-   
-x = 100000000000000000000
-y = 100000000000000000000
+travel(a, layout, 0)
+travel(b, layout, 1)
+  
+x = 2**30
+y = 2**30
 
 distance = x + y
 
 for i in layout:
-    #print(i, layout[i])
-    #continue
     if layout[i] == set([0,1]):
-        print(i, layout[i],abs(i[0]) + abs(i[1]))
         d = abs(i[0]) + abs(i[1])
         if d < distance:
             distance = d
+
 print(distance)
--- 3/3b.py
+++ 3/3b.py
@@ -3,72 +3,47 @@ f = open('input.txt').readlines()
 a = [(i[0],int(i[1:])) for i in f[0].split(',')]
 b = [(i[0],int(i[1:])) for i in f[1].split(',')]
 
-x = 0
-y = 0
+def travel(wire, layout, value, distance):
+    x = 0
+    y = 0
+    counter = 0
+    for i in wire:    
+        if i[0] == 'U':
+            for j in range(i[1]):
+                y += 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+                counter += 1
+                distance[(x,y)] = counter
+        elif i[0] == 'D':
+            for j in range(i[1]):
+                y -= 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+                counter += 1
+                distance[(x,y)] = counter
+        elif i[0] == 'L':
+            for j in range(i[1]):
+                x -= 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+                counter += 1
+                distance[(x,y)] = counter
+        elif i[0] == 'R':
+            for j in range(i[1]):
+                x += 1
+                layout[(x,y)] = layout.setdefault((x,y),set()).union(set([value]))
+                counter += 1
+                distance[(x,y)] = counter
 
 layout = {}
 distance_a = {}
 distance_b = {}
-
-counter_a = 0
 counter_b = 0
-for i in a:    
-    if i[0] == 'U':
-        for j in range(i[1]):
-            y += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-            counter_a += 1
-            distance_a[(x,y)] = counter_a
-    elif i[0] == 'D':
-        for j in range(i[1]):
-            y -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-            counter_a += 1
-            distance_a[(x,y)] = counter_a
-    elif i[0] == 'L':
-        for j in range(i[1]):
-            x -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-            counter_a += 1
-            distance_a[(x,y)] = counter_a
-    elif i[0] == 'R':
-        for j in range(i[1]):
-            x += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([0]))
-            counter_a += 1
-            distance_a[(x,y)] = counter_a
+
+travel(a, layout, 0, distance_a)
+travel(b, layout, 1, distance_b)
     
-x = 0
-y = 0
-    
-for i in b:
-    if i[0] == 'U':
-        for j in range(i[1]):
-            y += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-            counter_b += 1
-            distance_b[(x,y)] = counter_b
-    elif i[0] == 'D':
-        for j in range(i[1]):
-            y -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-            counter_b += 1
-            distance_b[(x,y)] = counter_b
-    elif i[0] == 'L':
-        for j in range(i[1]):
-            x -= 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-            counter_b += 1
-            distance_b[(x,y)] = counter_b
-    elif i[0] == 'R':
-        for j in range(i[1]):
-            x += 1
-            layout[(x,y)] = layout.setdefault((x,y),set()).union(set([1]))
-            counter_b += 1
-            distance_b[(x,y)] = counter_b
-   
+  
 # retrace a and b
-minimum = 100000000000000
+minimum = 2**30
 for i in [x for x in layout if layout[x] == set([0,1])]:
     m = distance_a[i] + distance_b[i]
     if m < minimum:
